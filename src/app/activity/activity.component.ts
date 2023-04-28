176ef0eba86e03b0f4b888730fb740d612845ceb
DESKTOP-NDU9AA6\casam <casamento.ignazio@gmail.com>
2023-04-29 00:48:08 +0200

    fix timer section, fix search section

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-activity',
@@ -7,18 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   activityTime = 25
   activitySecond: number = 0
   interval: any;
   clickedTimer = false;
+  canClickReset = false;
 
   ngOnInit(): void {
   }
 
   startTime() {
     this.clickedTimer = true;
+    this.canClickReset = true;
     this.interval = setInterval(() => {
       if (this.activityTime == 0 && this.activitySecond == 0) {
         clearInterval(this.interval);
@@ -34,7 +37,20 @@ export class ActivityComponent implements OnInit {
 
   stopTime() {
     this.clickedTimer = false;
-    clearInterval(this.interval)
+    clearInterval(this.interval);
+  }
+
+  stopTimeAndGo() {
+    this.clickedTimer = false;
+    clearInterval(this.interval);
+    this.activityTime = 25;
+    this.activitySecond = 0;
+    this.canClickReset = false;
+  }
+
+  goToBreak() {
+    this.stopTimeAndGo();
+    this.router.navigate(['break'])
   }
 
 }
--- src/app/break/break.component.ts
+++ src/app/break/break.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-break',
@@ -7,18 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreakComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   activityTime = 5
   activitySecond: number = 0
   interval: any;
   clickedTimer = false;
+  canClickReset = false;
 
   ngOnInit(): void {
   }
 
   startTime() {
     this.clickedTimer = true;
+    this.canClickReset = true;
     this.interval = setInterval(() => {
       if (this.activityTime == 0 && this.activitySecond == 0) {
         clearInterval(this.interval);
@@ -35,6 +38,19 @@ export class BreakComponent implements OnInit {
   stopTime() {
     this.clickedTimer = false;
     clearInterval(this.interval)
+  }
+
+  stopTimeAndGo() {
+    this.clickedTimer = false;
+    clearInterval(this.interval);
+    this.activityTime = 5;
+    this.activitySecond = 0;
+    this.canClickReset = false;
+  }
+
+  goToActivity() {
+    this.stopTimeAndGo();
+    this.router.navigate(['activity'])
   }
 
 }
